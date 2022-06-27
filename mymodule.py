c117789c597bad4e29496f851a5f13853338c825
eTonguzov <etonguzov@gmail.com>
2022-06-27 12:57:04 +0300

    Third commit

--- mymodule.py
+++ mymodule.py
@@ -1,4 +1,8 @@
+# Previosly I had some complex logic in this
+# module, but I had to delete it due to reasons.
+# Very sad!
+
 def my_function(first, second):
-    return first * second + 10
+    return first + second
 def another_function(first):
-    return first + 1u
+    return first + 1
